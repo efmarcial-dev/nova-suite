9d77f7c4ed7d718daf34550af71c0ffb0aa5bc39
efmarcial-dev <eduardo@novadev.solutions>
2025-12-04 11:54:53 -0500

    updated home cta section

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,4 +1,4 @@
-
+'use client'
 import HeroSection from "./components/Hero";
 import NewsLetterModal from "../components/NewsLetterModal";
 import ServiceSection from "./components/Services";
@@ -6,11 +6,13 @@ import WhySection from "./components/WhyUs";
 import CaseSection from "./components/CaseStudies";
 import FeatureSection from "./components/FeatureSection";
 import CTASec from "./components/CTA";
+import { CTASection } from "./components/CTASection";
+import { useRouter } from "next/navigation";
 
 
 export default function Home() {
 
-  
+  const router = useRouter();
 
   return (
     <>
@@ -21,7 +23,23 @@ export default function Home() {
         <FeatureSection />
         <WhySection />
         <CaseSection />
-        <CTASec />
+        {/* CTA */}
+        <section className="py-32 px-6 lg:px-20">
+          <div className="max-w-[1200px] mx-auto">
+            <CTASection
+              title="Join Us on This Journey"
+              description="Partner with a team that's committed to your success and passionate about technology innovation."
+              primaryButton={{
+                text: 'Work With Us',
+                onClick: () => router.push('/contact'),
+              }}
+              secondaryButton={{
+                text: 'View Our Work',
+                onClick: () => router.push('/case-studies'),
+              }}
+            />
+          </div>
+        </section>
     </div>
     </>
   );
