b93c31d3a243ac1532922dca88f278737c3c2fa3
efmarcial-dev <eduardo@novadev.solutions>
2025-12-10 03:05:20 -0500

    updated next config file

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  eslint: {
-    /**  ⛔️  Skip ESLint during `next build` (Vercel uses this command) */
-    ignoreDuringBuilds: true,
-  },
+  
+
 };
 
 export default nextConfig;
