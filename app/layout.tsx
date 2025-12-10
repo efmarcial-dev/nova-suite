476913410ca48843965e3b95a60f47dea71d7098
efmarcial-dev <eduardo@novadev.solutions>
2025-12-10 11:50:13 -0500

    added js

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" >
       <head>
-        <Script src='javascripts/jeeva.js'></Script>
+        <Script src='javascripts/jeeva.js' strategy="beforeInteractive"></Script>
       </head>
       <body className="h-full">{children}</body>
     </html>
